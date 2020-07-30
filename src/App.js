a57959d2327c8eb1ecce429a65fc7a640a8818e3
Ridwan <ridwanishola97@gmail.com>
2020-07-31 00:57:50 +0100

    complete setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Hello world</h1>
-            </div>
+            <BrowserRouter>
+                <div>
+                    <h1>Hello world</h1>
+                </div>
+            </BrowserRouter>
         );
     }
 }
